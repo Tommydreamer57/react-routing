ac4fde42b2a1a6300faa67bb0776d710162ccf9b
Thomas Lowry <minilao94@yahoo.com>
2017-09-19 12:31:06 -0600

    did project

--- src/components/Cart/Product/Product.js
+++ src/components/Cart/Product/Product.js
@@ -4,9 +4,11 @@ import './Product.css';
 
 export default function Product( { title, img, id } ) {
   return (
+    <Link id="CartProduct__navLink" to="../../Details/Details.js">
     <div id="CartProduct__container">
       <img alt="Product" src={ img } width="70px" height="70px" />
       <span id="CartProduct__title"> { title } </span>
     </div>
+    </Link>  
   )
 }
